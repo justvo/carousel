24e8984944304d7a311f2b8c7cde06798db4c733
justvo <vlad.otyan.02@gmail.com>
2024-02-01 16:47:55 +0200

    add swipes

--- carousel/src/App.jsx
+++ carousel/src/App.jsx
@@ -2,20 +2,49 @@ import React, { useState } from 'react'
 import './App.css'
 import Carousel from './components/Carousel'
 
+
 const images = [
-  'https://placekitten.com/800/400',
-  'https://placekitten.com/800/400',
-  'https://placekitten.com/800/401',
-  'https://placekitten.com/800/401',
-  'https://placekitten.com/800/406',
-  'https://placekitten.com/800/406',
+
+  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR1WspfBUI7WDyUZpbzFLT04QJ-lEUKgayxCQ&usqp=CAU',
+  'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c4/Eo_circle_red_number-1.svg/2048px-Eo_circle_red_number-1.svg.png',
+  'https://pngimg.com/uploads/number2/Number%202%20PNG%20images%20free%20download_PNG14925.png',
+  'https://lh3.googleusercontent.com/proxy/ETXOAldg7OsdNYHsY_-onTOZW-G4EhA19slST96apejm3sak85ep4jSARX17-jB9FBmZfLb63wdenEVDyHXT-AjPBOQYx1cSNb7ZIV4x',
+  'https://static.vecteezy.com/system/resources/previews/024/683/991/original/pink-3d-number-4-free-png.png',
+  'https://cdn-icons-png.flaticon.com/512/3840/3840754.png',
+  'https://www.thefactsite.com/wp-content/uploads/2021/06/number-6-facts-740x370.webp',
+  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSOzThXXbcFHGyKFSE_moY2DHhEVLugcFyRBQ&usqp=CAU',
+  'https://clipart-library.com/data_images/234031.png',
+  'https://img.freepik.com/premium-photo/fire-alphabet-number-9-nine-isolated-on-black-background_564276-8926.jpg',
+  'https://bank.gov.ua/admin_uploads/coin/d719773fbc33bd6c60891b8972e61f59.png',
 ];
 
 const App = () => {
- 
+
+  const [visible, setVisible] = useState(1);
+  const [swipes, setSwipes] = useState(1);
+
+  const handleVisible = (event) => {
+    setVisible(event.target.value); 
+  };
+  const handleSwipes = (event) => {
+    setSwipes(parseInt(event.target.value,10)); 
+  };
+
   return (
     <div className="app">
-        <Carousel images={images}  />
+      <Carousel images={images} visible={visible} swipes={swipes} />
+
+      <select value={visible} onChange={handleVisible}>
+        <option value="1">v 1</option>
+        <option value="2">v 2</option>
+        <option value="3">v 3</option>
+      </select>
+
+      <select value={swipes} onChange={handleSwipes}>
+        <option value="1">s 1</option>
+        <option value="2">s 2</option>
+        <option value="3">s 3</option>
+      </select>
 
 
     </div>
--- carousel/src/components/Carousel.jsx
+++ carousel/src/components/Carousel.jsx
@@ -1,15 +1,40 @@
-import React, { useState } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import '/src/styles/Carousel.css'
 
 const Carousel = ({ images, visible, swipes }) => {
 
     const [curr, setCurr] = useState(0);
+    const [startX, setStartX] = useState(null);
+    const carouselRef = useRef(null);
+    const [isDragging, setIsDragging] = useState(false);
 
-    visible = 3;
+    const [isAnimated, setIsAnimated] = useState(false);
+
+
+    // useEffect(() => {
+    //     const animate = () => {
+          
+    //       setIsAnimated(true);
+    
+    //       // Задержка для імітації тривалості анімації (може змінюватися відповідно до вашого коду)
+    //       setTimeout(() => {
+    //         // Ваш код, який виконується після завершення анімації
+    //         console.log('Анімація завершена');
+    //       }, 1000); // Припустимо, що анімація триває 1000 мс (1 секунда)
+    //     };
+    
+    //     animate();
+    //   }, []);
+
+
     const filteredImages = [];
+    let shift = visible == 2?-200:0;
+    
 
-    const customfilter = (visible, curr) => {
 
+
+    const customfilter = (curr) => {
+  
         filteredImages.push(images[curr]);
 
         for (let i = 1; i <= 4; i++) {
@@ -22,41 +47,104 @@ const Carousel = ({ images, visible, swipes }) => {
         }
     };
 
-    customfilter(visible, curr);
-    console.log(filteredImages)
+    const handleTouchStart = (e) => {
+        setStartX(e.touches[0].clientX);
+        setIsDragging(true);
+      };
+    
+      const handleTouchMove = (e) => {
+        if (isDragging && startX !== null) {
+          const deltaX = e.touches[0].clientX - startX + shift;
+          carouselRef.current.style.transition = 'none';
+          carouselRef.current.style.left = `${deltaX}px`;
+        }
+      };
+    
+      const handleTouchEnd = (e) => {
+        if (isDragging && startX !== null) {
+          setIsDragging(false);
+          const deltaX = e.changedTouches[0].clientX - startX;
+          carouselRef.current.style.transition = 'left 0.5s ease';
+    
+          if (deltaX > 50) {
+            prevSlide();
+          } else if (deltaX < -50) {
+            nextSlide();
+          } else {
+            carouselRef.current.style.left = `${shift}px`;
+          }
+        }
+    
+        setStartX(null);
+      };
 
+
+
+
+
+
+
+
+
+    // const handleTouchStart = (e) => {
+    //     setStartX(e.touches[0].clientX);
+
+    // };
+
+    // const handleTouchMove = (e) => {
+
+    //     if (startX !== null) {
+    //         const deltaX = e.touches[0].clientX - startX + shift;
+
+    //         carouselRef.current.style.left = `${deltaX}px`;
+    //       }
+    // };
+
+    // const handleTouchEnd = () => {
+
+    //     if(parseInt(carouselRef.current.style.left) > 50){
+
+    //         prevSlide()   ;
+    //     }
+    //     if(parseInt(carouselRef.current.style.left) < -50){
+    //         nextSlide();
+    //     }
+    //     setStartX(null);
+    //     carouselRef.current.style.left = `${shift}px`;
+    // };
 
 
     const nextSlide = () => {
         setCurr((prevIndex) => {
-            prevIndex++;
+            prevIndex = prevIndex + swipes;
             if (prevIndex >= images.length) {
-                prevIndex = 0;
+                prevIndex = prevIndex - images.length;
             }
 
+            customfilter(prevIndex);
             return prevIndex;
         })
+
     };
 
 
     const prevSlide = () => {
         setCurr((prevIndex) => {
-            prevIndex--;
-            if (prevIndex <= 0) {
-                prevIndex = images.length - 1;
+            prevIndex = prevIndex - swipes;
+            if (prevIndex < 0) {
+                prevIndex = images.length - swipes;
             }
 
+            customfilter(prevIndex);
             return prevIndex;
         })
+
 
     };
 
 
-
-
-    const prevIm = (curr - 1) < 0 ? images.length - 1 : curr - 1;
-    const nextIm = (curr + 1) > images.length - 1 ? 0 : curr + 1;
-    console.log((curr - 1) < 0);
+    //main
+    customfilter(curr);
 
     return (
         <div>
@@ -64,10 +152,16 @@ const Carousel = ({ images, visible, swipes }) => {
             <button className='next-button' onClick={prevSlide}> prev </button>
             <button className='next-button' onClick={nextSlide}> next </button>
             <div className="carousel">
-                <div className='images'>
+                <div
+                    className='images'
+                    style={{ position: 'relative', left: `${shift}px` }}
+                    ref={carouselRef}
+                    onTouchStart={handleTouchStart}
+                    onTouchMove={handleTouchMove}
+                    onTouchEnd={handleTouchEnd}>
                     {filteredImages.map((image, index) => (
                         <div>
-                        <img className={'image'} width={`${780 / visible }px`}  key={index} src={image} alt={`Image ${index + 1}`} />
+                            <img className={'image'} width={`${780 / visible}px`} key={index} src={image} alt={`Image ${index + 1}`} />
                         </div>
                     ))}
                 </div>
